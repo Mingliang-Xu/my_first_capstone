a94a522011433fb0c1037dbb65cacc2e1f7a9f38
Mingliang-Xu <oliver.xml@hotmail.com>
2023-10-31 08:54:37 -0700

    added AWS EC2 deployment

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(`${__dirname}/public`));
+
 const capstonedb = require("./database");
 const seed = require("./seed");
 
